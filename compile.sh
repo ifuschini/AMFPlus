fee0735c909bc6a6677234bdb49505cbc18914b6
Idel Fuschini <idel.fuschini@double77.com>
2018-04-09 12:48:04 +0200

    deleted

--- compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "Please set"
-
-#sudo /opt/local/apache2/bin/apxs -c -a -i mod_amf51.c
-sudo /opt/local/apache2/bin/apxs -c -a -i mod_amf.c -lcurl 
-#sudo /opt/local/apache24/bin/apxs -c -a -i mod_amf.c -lcurl 
-#sudo /opt/local/apache2/bin/apxs -i -a -L/usr/local/lib -I/usr/local/include -c mod_amf.c
