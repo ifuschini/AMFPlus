0c352e2c1f49769ee60cd4d52fd66fd6ae707ef9
IRT User <irtuser@eng-irt09-v20.ocio.monash.edu>
2018-05-21 14:24:10 +1000

    Fixed minor issues in the install script, renamed the header file as required and added detail to the README.

--- install.sh
+++ install.sh
@@ -15,47 +15,45 @@ var=0;
 while [ $var -eq 0 ]
 do
     myapxs=`which apxs2 || which apxs`
-    echo "Found apxs/apxs2: $myapxs"
-    echo "Please give the path to apxs/apxs2 [$myapxs]:"
+    #echo "Found apxs/apxs2: $myapxs"
+    echo "Please confirm the path to apxs/apxs2 [$myapxs]:"
     read apxs
     if [ -z $apxs ]; then
         apxs=$myapxs
     fi
     if [ -f $apxs ]; then
-            echo "apxs file exists"           
-            echo "Check libcurl exist....."
-            CURL_VERSION=$(cur-config --version 2> /dev/null)
-            if [ $? -eq 0 ]; then
-                echo "Libcurl Version is $CURL_VERSION"
-                cp mod_amf.tmpl mod_amf.h
-                RUN_SUDO=$(sudo $apxs -c -a -i mod_amf.c -lcurl)
-                if [ ${RUN_SUDO} -eq 0 ]
-                then
-                    $apxs -c -a -i mod_amf.c -lcurl
-                fi
-            else
-                echo "Libcurl not found"
-                grep -v "#define CURL_SUPPORT" mod_amf.tmpl > mod_amf.h
-                RUN_SUDO=$(sudo $apxs -c -a -i mod_amf.c)
-                if [ ${RUN_SUDO} -eq 0 ]
-                then
-                    $apxs -c -a -i mod_amf.c
-                fi
-                echo "Remember you mus configure manually in your httpd.conf this parameters downloadable from (https://sourceforge.net/projects/mobilefilter/files/AMFPlusRepository/):"
-                echo
-                echo "AMFmobile from litemobiledetectionPlus.config file"
-                echo
-                echo "AMFtouch from litetabletdetectionPlus.config file"
-                echo
-                echo "AMFtablet from litetouchdetectionPlus.config file"
-                echo
-                echo "AMFtv from litetvdetectionPlus.config file"
-                echo
-                echo "If you don't wont to configure it install the libcurl from https://curl.haxx.se/libcurl/ and rerun the amf installation script, and AMF download automatically every time"
+        echo "apxs exists"           
+        echo "Checking if libcurl exists....."
+        CURL_VERSION=$(curl-config --version 2> /dev/null)
+        if [ $? -eq 0 ]; then
+            echo "Libcurl version is $CURL_VERSION"
+            cp mod_amf.tmpl mod_amf.h
+            RUN_SUDO=$(sudo $apxs -c -a -i mod_amf.c -lcurl)
+            if [ ${RUN_SUDO} -eq 0 ]; then
+                $apxs -c -a -i mod_amf.c -lcurl
             fi
-        var=1
         else
-            echo "File does not exist $apxs\n\nPlease try again !!!!!!\n"
-            var=0
+            echo "Libcurl not found"
+            grep -v "#define CURL_SUPPORT" mod_amf.tmpl > mod_amf.h
+            RUN_SUDO=$(sudo $apxs -c -a -i mod_amf.c)
+            if [ ${RUN_SUDO} -eq 0 ]; then
+                $apxs -c -a -i mod_amf.c
+            fi
+            echo "Remember you must manually configure your httpd.conf with these parameters (downloadable from https://sourceforge.net/projects/mobilefilter/files/AMFPlusRepository/):"
+            echo
+            echo "AMFmobile from litemobiledetectionPlus.config file"
+            echo
+            echo "AMFtouch from litetabletdetectionPlus.config file"
+            echo
+            echo "AMFtablet from litetouchdetectionPlus.config file"
+            echo
+            echo "AMFtv from litetvdetectionPlus.config file"
+            echo
+            echo "If you don't wont to configure it, install libcurl from https://curl.haxx.se/libcurl/ and rerun the AMF installation script. AMF will update automatically every time."
+        fi
+        var=1
+    else
+        echo "File does not exist: $apxs\n\nPlease try again!\n"
+        var=0
     fi
 done
